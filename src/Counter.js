aa58581e7fb026007fa058e431fe2972bf32ad2f
njha32 <neha.jha@dxc.com>
2025-05-10 12:09:36 +0530

    image corousal and simple countapp implementation

---
+++ src/Counter.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+
+const Counter = () => {
+    const [count,setCount]=useState(0);
+  return (
+    <div>
+        <p>Count:{count}</p>
+        <button className="btn"onClick={()=>{
+            setCount(count+1)
+        }}>Increment</button>
+    </div>
+  )
+}
+
+export default Counter
---
+++ src/ImageSlider.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+import "./index.css"
+const data=["https://wallhalla.com/thumbs/6","https://wallhalla.com/thumbs/16","https://wallhalla.com/thumbs/40","https://wallhalla.com/thumbs/13"];
+const ImageSlider = () => {
+    const[activeImageIndex,setActiveImageIndex]=useState(0)
+    const handlePreviousClick=()=>{
+        if(activeImageIndex === 0){setActiveImageIndex(data.length-1)}
+        else{
+        setActiveImageIndex(activeImageIndex-1)
+        }
+    }
+    const handleNextClick=()=>{
+        setActiveImageIndex((activeImageIndex+1)%data.length)
+    }
+  return (
+  activeImageIndex !== undefined ? (
+    <div className="main">
+      <button className="btn1" onClick={handlePreviousClick}>Previous</button>
+      <div>
+        <img
+          className="imgg"
+          src={data[activeImageIndex]}
+          alt="Not able to download url"
+        />
+      </div>
+      <button className="btn2" onClick={handleNextClick}>Next</button>
+    </div>
+  ) : null
+);
+
+}
+
+export default ImageSlider
