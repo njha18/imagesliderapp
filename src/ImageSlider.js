5f5336e83c35c4281d6eef16401f28742da87dc7
njha32 <neha.jha@dxc.com>
2025-05-10 12:45:28 +0530

    implement css

--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,33 +1,44 @@
-import React, { useState } from 'react'
-import "./index.css"
-const data=["https://wallhalla.com/thumbs/6","https://wallhalla.com/thumbs/16","https://wallhalla.com/thumbs/40","https://wallhalla.com/thumbs/13"];
+import React, { useState } from "react";
+import "./index.css";
+const data = [
+  "https://wallhalla.com/thumbs/6",
+  "https://wallhalla.com/thumbs/16",
+  "https://wallhalla.com/thumbs/40",
+  "https://wallhalla.com/thumbs/13",
+];
 const ImageSlider = () => {
-    const[activeImageIndex,setActiveImageIndex]=useState(0)
-    const handlePreviousClick=()=>{
-        if(activeImageIndex === 0){setActiveImageIndex(data.length-1)}
-        else{
-        setActiveImageIndex(activeImageIndex-1)
-        }
+  const [activeImageIndex, setActiveImageIndex] = useState(0);
+  const handlePreviousClick = () => {
+    if (activeImageIndex === 0) {
+      setActiveImageIndex(data.length - 1);
+    } else {
+      setActiveImageIndex(activeImageIndex - 1);
     }
-    const handleNextClick=()=>{
-        setActiveImageIndex((activeImageIndex+1)%data.length)
-    }
-  return (
-  activeImageIndex !== undefined ? (
+  };
+  const handleNextClick = () => {
+    setActiveImageIndex((activeImageIndex + 1) % data.length);
+  };
+  return activeImageIndex !== undefined ? (
     <div className="main">
-      <button className="btn1" onClick={handlePreviousClick}>Previous</button>
+      <button className="btn1" onClick={handlePreviousClick}>
+        Previous
+      </button>
       <div>
-        <img
-          className="imgg"
-          src={data[activeImageIndex]}
-          alt="Not able to download url"
-        />
+        {data.map((url, index) => (
+          <img
+            className={`imgg ${
+              index === activeImageIndex ? "display-block" : "display-none"
+            }`}
+            src={url}
+            alt="Not able to download url"
+          />
+        ))}
       </div>
-      <button className="btn2" onClick={handleNextClick}>Next</button>
+      <button className="btn2" onClick={handleNextClick}>
+        Next
+      </button>
     </div>
-  ) : null
-);
+  ) : null;
+};
 
-}
-
-export default ImageSlider
+export default ImageSlider;
